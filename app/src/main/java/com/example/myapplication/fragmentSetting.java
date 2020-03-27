f112de5ad50c570c30d0a99114be324698f3727f
helathThief <celena1004@naver.com>
2020-03-27 16:08:27 +0900

    3/27 - Profile Update Complete

--- app/src/main/java/com/example/myapplication/fragmentSetting.java
+++ app/src/main/java/com/example/myapplication/fragmentSetting.java
@@ -40,13 +40,12 @@ public class fragmentSetting extends Fragment {
         Log.d("Count",String.valueOf(cursor.getCount()));
         if(cursor.getCount()>0){
             cursor.moveToFirst();
-            String s="0";
             nameT.setText(cursor.getString(0));
             ageT.setText(cursor.getString(1));
             weightT.setText(cursor.getString(2));
             heightT.setText(cursor.getString(3));
-            s = cursor.getString(4);
-            if (s=="1") {sexT.setText("남");} else {sexT.setText("여");}
+            String s = cursor.getString(4);
+            if (s.equals("1")) {sexT.setText("남");} else if(s.equals("2")) {sexT.setText("여");} else{sexT.setText("미정");}
             scalT.setText(cursor.getString(5));
         }
 
