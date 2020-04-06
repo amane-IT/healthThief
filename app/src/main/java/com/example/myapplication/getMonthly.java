dcc42a4823c709f310f9bf994867f91ec9af2eb8
amane-IT <gyp03236@naver.com>
2020-04-06 13:49:03 +0900

    4월 6일  - 머신러닝 50가지 추가  - 합침

--- app/src/main/java/com/example/myapplication/getMonthly.java
+++ app/src/main/java/com/example/myapplication/getMonthly.java
@@ -3,8 +3,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarMode;
@@ -14,18 +18,24 @@ import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
-public class getMonthly extends AppCompatActivity {
+
+public class getMonthly extends AppCompatActivity{
 
     getMonthly gm = this;
     Button back;
     MaterialCalendarView calendarView;
     ArrayList<Diary> diaries;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.report_month);
+
+
 
         // 전 화면으로 돌아감
         back = (Button) findViewById(R.id.monthBack);
@@ -40,12 +50,15 @@ public class getMonthly extends AppCompatActivity {
             }
         });
 
+
+
         // https://github.com/prolificinteractive/material-calendarview
         // https://dpdpwl.tistory.com/3
+        // https://github.com/prolificinteractive/material-calendarview/issues/617
         calendarView = findViewById(R.id.calendarView);
         calendarView.state().edit()
                 .setFirstDayOfWeek(Calendar.SUNDAY)
-                .setMinimumDate(CalendarDay.from(2020, 2, 1))
+                .setMinimumDate(CalendarDay.from(2020, 3, 1))
                 .setMaximumDate(CalendarDay.from(2030, 11, 31))
                 .setCalendarDisplayMode(CalendarMode.MONTHS)
                 .commit();
@@ -63,6 +76,19 @@ public class getMonthly extends AppCompatActivity {
         /**
          * 넘겨주고 싶다!! 넘겨주고 싶ㅍ다!!
          */
+
+        // dialog 사이즈 조정
+        // https://wimir-dev.tistory.com/11
+        DisplayMetrics dm = getApplicationContext().getResources().getDisplayMetrics();
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        MonthlyDialog mdialog = new MonthlyDialog(gm);
+        mdialog.setCancelable(false);
+        WindowManager.LayoutParams wm = mdialog.getWindow().getAttributes();
+        wm.copyFrom(mdialog.getWindow().getAttributes());
+        wm.width = width/2;
+        wm.height = height/2;
+
         calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
             @Override
             public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
@@ -71,19 +97,21 @@ public class getMonthly extends AppCompatActivity {
                 // MonthlyDialog로 해당 날짜의 db 값 넘겨줌
                 // https://hyongdoc.tistory.com/184 - arraylist 넘겨주기
                 // https://injunech.tistory.com/241
+
+                //위에꺼 안됨... 이걸로 다시 시도
+                //https://stackoverflow.com/questions/6124989/pass-data-from-activity-to-dialog/42511882
                 int dateToDialog = Integer.parseInt(format.format(date.getDate()));
                 diaries = DBHelper.getDiaryDataByDate(dateToDialog);
                 Intent it = new Intent(gm, MonthlyDialog.class);
                 it.putExtra("diaries", diaries);
 
-                MonthlyDialog mdialog = new MonthlyDialog(gm);
-                mdialog.setCancelable(false);
+
                 mdialog.show();
 
             }
         });
 
-        //   https://github.com/Applandeo/Material-Calendar-View
+
 
         // 다이어리db에서 날짜 기반 데이터(1달) 가져와 칼로리 및 영양소 분석
         // 특정 날짜에 컬러색 칠하는 함수 만들기... 각 컬러 갯수 return (int형)
@@ -92,5 +120,4 @@ public class getMonthly extends AppCompatActivity {
         //클릭하면 그 날짜에 먹은 메뉴? 다이어리 팝업? 고민 중...
 
     }
-
 }
--- app/src/main/java/com/example/myapplication/getTrend.java
+++ app/src/main/java/com/example/myapplication/getTrend.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.animation.Easing;
@@ -16,8 +17,9 @@ import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
+
+import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -29,11 +31,11 @@ public class getTrend extends AppCompatActivity {
     getTrend gt =this;
     Button back;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.report_trend);
-
 
         // 전 화면으로 돌아감
         back = (Button) findViewById(R.id.trendBack);
@@ -48,12 +50,19 @@ public class getTrend extends AppCompatActivity {
                 finish();
             }
         });
-        // TODO : get data from Diary DB
+
+        // get data from Diary DB
         // 하루동안 섭취한 탄/단/지를 원그래프로 확인
         // 부족한 영양소를 setText 로 알려줌 (탄단지 비율 5:3:2)
 
-        TextView advice = (TextView) findViewById(R.id.giveAdvice);
-        TextView nutri_advice = (TextView) findViewById(R.id.giveAdvice_nut);
+        //TextView advice = (TextView) findViewById(R.id.giveAdvice);
+        //TextView nutri_advice = (TextView) findViewById(R.id.giveAdvice_nut);
+
+
+        float carbon = 0;
+        float protein =0;
+        float fat = 0;
+        float total_cal = 0;
 
         SimpleDateFormat format = new SimpleDateFormat ("yyyyMMdd");
         Calendar time = Calendar.getInstance();
@@ -61,37 +70,39 @@ public class getTrend extends AppCompatActivity {
 
         DbHelper DBHelper = new DbHelper(this,"TEST",null,DbHelper.DB_VERSION);
         SQLiteDatabase db = DBHelper.getReadableDatabase();
-        Cursor cursor = db.rawQuery(" SELECT CARBO, PROTEIN, FAT FROM FOODIARYDB WHERE DATE ="+date,null);
-        float carbon = 0;
-        float protein =0;
-        float fat = 0;
-        int total_cal = 0;
+        Cursor cursor = db.rawQuery(" SELECT CARBO, PROTEIN, FAT, CAL FROM FOODIARYDB WHERE DATE ="+date,null);
+
 
         if(cursor.getCount()>0){
-            // TODO : 섭취 탄단지 합계 구하기
+            // 섭취 탄단지 합계 구하기
             while(cursor.moveToNext()){
                 carbon += Float.valueOf(cursor.getString(0));
                 protein += Float.valueOf(cursor.getString(1));
                 fat += Float.valueOf(cursor.getString(2));
-                total_cal = (int)carbon*4 + (int)protein*4 + (int)fat*9;
+                total_cal += Float.valueOf(cursor.getString(3));
+                //total_cal = (int)carbon*4 + (int)protein*4 + (int)fat*9;
             }
-            // TODO : 부족한 영양소 찾기 + 칼로리 섭취량 확인
-            Cursor info_c = db.rawQuery("SELECT WEIGHT, SCAL FROM INFODB",null);
+            // 부족한 영양소 찾기 + 칼로리 섭취량 확인
+            Cursor info_c = db.rawQuery("SELECT SCAL FROM INFODB",null);
             info_c.moveToFirst();
-            int weight = info_c.getInt(0);
-            int scal = info_c.getInt(1);
+            int scal;
+            scal = info_c.getInt(0);
 
             // 1. 섭취 칼로리 과다/부족 확인 - 탄단지 열량 합산 & info 디비의 scal 과 비교 - 추후 범위 수정
+            /*
             String calString = String.valueOf(total_cal);
             String lessAdvice = calString+"kcal : 더 마니 머겅" ;
             String muchAdvice = calString+"kcal : 너무 마니 머거슴";
             if (scal>total_cal) advice.setText(lessAdvice);
             else advice.setText(muchAdvice);
+             */
 
             // 2. 탄단지 맞춰 섭취했는지 확인 - scal 이용 이상적인 탄단지 섭취량 확인 & 비교 - 추후 범위 수정
             int proper_c = (int)(scal*0.5/4);
             int proper_p = (int)(scal*0.3/4);
             int proper_f = (int)(scal*0.2/9);
+
+            /*
             String cadvice = "";
             String padvice = "";
             String fadvice = "";
@@ -100,17 +111,79 @@ public class getTrend extends AppCompatActivity {
             if (proper_f <= fat) fadvice = "과다"; else fadvice = "부족";
             String nutri_total_advice = "탄수화물: " + cadvice +  " 단백질: " + padvice + "지방:"+fadvice;
             nutri_advice.setText(nutri_total_advice);
+             */
+
+            // TODO: ProgressBar get Nutrient Data from FOODIARYDB
+
+            TextView kcalS = (TextView) findViewById(R.id.kcalStart);
+            TextView carboS = (TextView) findViewById(R.id.carboStart);
+            TextView proS = (TextView) findViewById(R.id.proteinStart);
+            TextView fatS = (TextView) findViewById(R.id.fatStart);
+            TextView kcalE = (TextView) findViewById(R.id.kcalEnd);
+            TextView carboE = (TextView) findViewById(R.id.carboEnd);
+            TextView proE = (TextView) findViewById(R.id.proteinEnd);
+            TextView fatE = (TextView) findViewById(R.id.fatEnd);
+            ProgressBar kcalP = (ProgressBar) findViewById(R.id.kcalProgress);
+            ProgressBar carboP = (ProgressBar) findViewById(R.id.carbonProgress);
+            ProgressBar proP = (ProgressBar) findViewById(R.id.proteinProgress);
+            ProgressBar fatP = (ProgressBar) findViewById(R.id.fatProgress);
+
+            int kcalPer = (int)((total_cal/scal)*100);
+            int carboPer = (int)((carbon/proper_c)*100);
+            int proPer = (int)((protein/proper_p)*100);
+            int fatPer = (int)((fat/proper_f)*100);
+
+
+            kcalP.setProgress(kcalPer);
+            carboP.setProgress(carboPer);
+            proP.setProgress(proPer);
+            fatP.setProgress(fatPer);
+
+            kcalS.setText(String.valueOf(total_cal));
+            carboS.setText(String.valueOf(carbon));
+            proS.setText(String.valueOf(protein));
+            fatS.setText(String.valueOf(fat));
+
+            kcalE.setText(String.valueOf(scal));
+            carboE.setText(String.valueOf(proper_c));
+            proE.setText(String.valueOf(proper_p));
+            fatE.setText(String.valueOf(proper_f));
+
+            Cursor progressStart = db.rawQuery(" SELECT CAL, CARBO, PROTEIN, FAT FROM FOODIARYDB WHERE DATE ="+date,null);
+            Cursor info = db.rawQuery("SELECT SCAL FROM INFODB",null);
+
+
         }
         else{
             carbon = 20f;
             protein = 20f;
             fat = 10f;
-            advice.setText("먹은 게 없엉");
-            nutri_advice.setText("탄단지도 없엉");
+            //advice.setText("먹은 게 없엉");
+            //nutri_advice.setText("탄단지도 없엉");
 
         }
 
-        // TODO : making pieChart
+
+
+
+        /*
+        Log.d("Count",String.valueOf(cursor.getCount()));
+        if(cursor.getCount()>0){
+            cursor.moveToFirst();
+            String s="0";
+            nameT.setText(cursor.getString(0));
+            ageT.setText(cursor.getString(1));
+            weightT.setText(cursor.getString(2));
+            heightT.setText(cursor.getString(3));
+            s = cursor.getString(4);
+            if (s=="1") {sexT.setText("남");} else {sexT.setText("여");}
+            scalT.setText(cursor.getString(5));
+        }
+        cursor.close();
+         */
+
+
+        // making pieChart
         pieChart = (PieChart)findViewById(R.id.piechart);
 
         // 퍼센티지
@@ -126,8 +199,8 @@ public class getTrend extends AppCompatActivity {
         pieChart.setHoleColor(Color.WHITE);
         pieChart.setTransparentCircleRadius(35f);
         // 애니메이션
-        pieChart.animateXY(5000,3000);
-        pieChart.animateY(1000, Easing.EasingOption.EaseInOutCubic);
+        //pieChart.animateXY(5000,3000);
+        //pieChart.animateY(1000, Easing.EasingOption.EaseInOutCubic);
 
 
         ArrayList<PieEntry> yValues = new ArrayList<PieEntry>();
@@ -160,9 +233,7 @@ public class getTrend extends AppCompatActivity {
 
         pieChart.setData(data);
 
-        // db에서 제일 최근에 먹은 음식 3일분 어치 가져오기
-        // db에서  현재 날짜에서 1달 단위로 많이 먹은 음식 가져오기
-        // db에서 1달치 영양소 평균으로 평가 내려주기 (ex) 단백질이 부족해요! 오늘은 고기 한판...? )
+        // 당일 먹은 음식 탄단지 원그래프로 알려주고 부족한 영양소 알려주기
 
     }
 }
